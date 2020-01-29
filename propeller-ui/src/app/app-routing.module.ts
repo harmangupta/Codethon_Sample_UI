8b77d4244745d92218d7f2f42a48826759179c8c
charvp <charvak.patel@morganstanley.com>
2020-01-29 10:17:39 +0000

    Base Frame

--- propeller-ui/src/app/app-routing.module.ts
+++ propeller-ui/src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { RequestAccessComponent } from './AppComponent/request-access/request-ac
 
 
 export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'requestAccess', component: RequestAccessComponent }
